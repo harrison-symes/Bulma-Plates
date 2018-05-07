ddcfa180735d93522b44288acd5901bfdaa9b3c3
harrison-symes <symeshjb@gmail.com>
2018-05-08 10:52:34 +1200

    fixed an issue

--- client/components/Birds.jsx
+++ client/components/Birds.jsx
@@ -4,16 +4,20 @@ import {connect} from 'react-redux'
 
 import {deleteBird} from '../actions/birds'
 
-const Birds = ({birds, dispatch}) => (
+const Birds = ({birds, del}) => (
   <div className="columns">
     {birds.map(({id, name, image_url}) => <div className="box column is-4 has-text-centered" key={id}>
       <h1 className="subtitle is-2">{name}</h1>
       <img className="image" src={image_url} />
-      <button className="delete" onClick={() => dispatch(deleteBird(id)) }>Del</button>
+      <button className="delete" onClick={() => del(id)}>Del</button>
     </div>)}
   </div>
 )
 
 const mapStateToProps = ({birds}) => ({birds})
 
-export default connect(mapStateToProps)(Birds)
+const mapDispatchToProps = (dispatch) => ({
+  del: id => dispatch(deleteBird(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Birds)
